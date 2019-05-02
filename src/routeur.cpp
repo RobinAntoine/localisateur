c997a69d462639320bc32cf2f85e01ac5f8deec9
ROBIN Antoine <antoine.robin01@gmail.com>
2019-05-02 21:03:18 +0200

    add the input canal from the routeur

--- src/routeur.cpp
+++ src/routeur.cpp
@@ -1,17 +1,23 @@
 #include <ros/ros.h>
 #include <iostream>
+#include <std_msgs/ByteMultiArray.h>
 
 using namespace std;
 
+void inputCallBack (const std_msgs::ByteMultiArray& msg)
+{
+	cout	<<"Hello"<<endl;
+}
 
 int main(int argc, char **argv)
 {
 
-	
+	//initialization of the ros node
 	ros::init(argc, argv,"routeur");
 	ros::NodeHandle n;
 
-	//ros::Subscriber image_sub_ = n.subscribe("/camera/rgb/image_color", 1,imageCb);
+	//add the input topic
+	ros::Subscriber image_sub_ = n.subscribe("/emissionCanal", 100,inputCallBack);
 
 	ros::Rate r(20);
 
