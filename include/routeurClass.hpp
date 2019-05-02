9b18a83716906d10ed75abb2b9e7a4a30b5e8400
ROBIN Antoine <antoine.robin01@gmail.com>
2019-05-02 21:29:44 +0200

    add the router class

---
+++ include/routeurClass.hpp
@@ -0,0 +1,12 @@
+#ifdef ROUTEUR_CLASSE
+#define ROUTEUR_CLASSE
+
+#include <iostream>
+#include <std_msgs/ByteMultiArray.h>
+#include <vector.h>
+
+using namespace std;
+
+
+
+#endif
---
+++ src/routeurClass.cpp
@@ -0,0 +1,1 @@
+#include "routeurClass.hpp"
